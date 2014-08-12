a928f34b0b9877a460bfbc1c5c6ef8b5df93b83b
Jonathan Hartman <j@p4nt5.com>
2014-08-12 16:51:10 -0700

    Initial commit; skeleton generated by Chef-DK

---
+++ README.md
@@ -0,0 +1,4 @@
+# hipache
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'hipache'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures hipache'
+long_description 'Installs/Configures hipache'
+version          '0.1.0'
+
