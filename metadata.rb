09342271d38248955eea1735b3899cd956ba62a6
Jonathan Hartman <j@p4nt5.com>
2014-08-18 15:14:18 -0700

    Bump version for dev

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'j@p4nt5.com'
 license          'Apache v2.0'
 description      'Installs/Configures hipache'
 long_description 'Installs/Configures hipache'
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'nodejs'
 
